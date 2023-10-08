c0613d4a4c24107e201d69eb65f84098c6df919a
dttrue <danieltorres@pursuit.org>
2023-10-07 20:29:13 -0400

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./assets/**/*.liquid",
+            "./layout/**/*.liquid",
+            "./sections/**/*.liquid",
+            "./templates/customers/**/*.liquid"],
+
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
